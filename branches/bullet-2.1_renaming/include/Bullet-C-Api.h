67e220e62feb19fe11bebaa0487e0ee7bd35ce59
erwin.coumans <erwin.coumans@08e121b0-ae19-0410-a57b-3be3395fd4fd>
2009-12-22 00:13:50 +0000

    removed obsolete tree
    git-svn-id: http://bullet.googlecode.com/svn@1866 08e121b0-ae19-0410-a57b-3be3395fd4fd

--- branches/bullet-2.1_renaming/include/Bullet-C-Api.h
+++
@@ -1,9 +0,0 @@
-
-///This is just a placeholder for now.
-///
-///It will contain a C API for high-level Bullet Collision Detection and Physics library.
-///
-///for now, only C++ interface is available, see btBulletCollisionCommon.h and btBulletDynamicsCommon.h
-///
-///http://bullet.sf.net
-	
